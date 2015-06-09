8a0a5658bc35e12e0d6b8dd56226e870e76e2cbb
Henrik Bergvin <henrik@bergvin.com>
2015-06-09 11:27:50 +0200

    Cleanup for release

--- TechKonstraints.cs
+++ TechKonstraints.cs
@@ -36,27 +36,18 @@ namespace TechKonstraints
 				return; // just break, there's no need to do anything here if you don't purchase parts.
 			}
 
-			Utilities.Log ("TechKonstraints", GetInstanceID (), "Part purchased: " + part.name + " (" + part.configFileFullName + ")");
 			// Look for partRequired in the AvailablePart config
 			ConfigNode partConfig = part.partConfig;
 
 			if (partConfig.HasValue("requiredParts")) {
-				Utilities.Log ("TechKonstraints", GetInstanceID (), "Part " + part.name + " has the following requiredParts: ");
-
 				string requiredParts = partConfig.GetValue ("requiredParts");
-
-				Utilities.Log ("TechKonstraints", GetInstanceID (), requiredParts);
 
 				// OK! We've got some required parts. Now, is this a list?
 				List<string> partsRequired = requiredParts.Split (',').ToList ();
 				bool canResearch = true;
 
-				Utilities.Log ("TechKonstraints", GetInstanceID (), "Size of partsRequired is " + partsRequired.Count);
-
 				foreach (string rPart in partsRequired)
 				{
-					Utilities.Log ("TechKonstraints", GetInstanceID (), "Part '" + rPart.Trim() + "' is required. Searching...");
-
 					bool partFound = false;
 
 					foreach (RDNode node in RDController.Instance.nodes) {
@@ -67,7 +58,6 @@ namespace TechKonstraints
 								continue;
 							foreach (AvailablePart ap in protoNode.partsPurchased) {
 								if (ap.name == rPart.Trim()) {
-									Utilities.Log ("TechKonstraints", GetInstanceID (), "Part " + ap.name + " is purchased.");
 									partFound = true;
 									break;
 								}
@@ -82,7 +72,6 @@ namespace TechKonstraints
 
 					if (!partFound)
 					{
-						Utilities.Log ("TechKonstraints", GetInstanceID (), "We can't research this!");
 						canResearch = false;
 						ScreenMessages.PostScreenMessage ("You must purchase " + GetPartNameFromID (rPart.Trim ()) + " first!", 5, ScreenMessageStyle.UPPER_CENTER);
 						break;
@@ -90,24 +79,17 @@ namespace TechKonstraints
 				}
 
 				if (!canResearch) {
-					Utilities.Log ("TechKonstraints", GetInstanceID (), "Time for cleanup, removing researched part.");
-					Utilities.Log ("TechKonstraints", GetInstanceID (), "Finding " + part.TechRequired);
-
 					RDNode node = RDController.Instance.nodes.Find (n => n.tech.techID == part.TechRequired);
 					if (node)
 					{
-						Utilities.Log ("TechKonstraints", GetInstanceID (), "In tech node " + node.name);
 						ProtoTechNode protoNode = ResearchAndDevelopment.Instance.GetTechState (node.tech.techID);
 						if (protoNode.partsPurchased.Contains (part))
 						{
-							Utilities.Log ("TechKonstraints", GetInstanceID (), "Removing part from list of purchased parts.");
 							protoNode.partsPurchased.Remove (part);
 							Funding.Instance.AddFunds (part.entryCost, TransactionReasons.RnDPartPurchase); // give back money!
 						}
 					}
 				}
-
-
 			}
 		}
 
